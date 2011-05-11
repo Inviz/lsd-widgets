7a528e22f1c1a48e5de4949f42edaa39128b8a24
Yaroslaff Fedin <invizko@gmail.com>
2011-05-11 21:51:36 +0800

    Include wysiwyg widget definition

--- Source/Input/HTML.js
+++ Source/Input/HTML.js
@@ -1,3 +1,24 @@
+/*
+---
+ 
+script: HTML.js
+ 
+description: Wysiwyg for people
+ 
+license: Public domain (http://unlicense.org).
+
+authors: Yaroslaff Fedin
+ 
+requires:
+  - LSD.Widget.Input
+  - CKEditor/core._bootstrap
+
+provides: [LSD.Widget.Input.HTML]
+ 
+...
+*/
+
+
 LSD.Widget.Input.HTML = new Class({
   
 })
