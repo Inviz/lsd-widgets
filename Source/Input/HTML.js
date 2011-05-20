77c19b43dc381d7a07d671f3755e1d190fbe5502
Yaroslaff Fedin <invizko@gmail.com>
2011-05-20 19:08:34 +0800

    Bring HTML widget here

--- Source/Input/HTML.js
+++ Source/Input/HTML.js
@@ -11,7 +11,7 @@ authors: Yaroslaff Fedin
  
 requires:
   - LSD.Widget.Input
-  - CKEditor/core._bootstrap
+  - LSD/LSD.Mixin.ContentEditable
 
 provides: [LSD.Widget.Input.HTML]
  
@@ -20,5 +20,41 @@ provides: [LSD.Widget.Input.HTML]
 
 
 LSD.Widget.Input.HTML = new Class({
+  options: {
+    tag: 'htmlarea',
+    writable: true,
+    attributes: {
+      contenteditable: 'editor'
+    },
+    states: {
+      editing: {
+        enabler: 'edit',
+        disable: 'finish'
+      }
+    },
+    events: {
+      element: {
+        click: 'edit'
+      },
+      self: {
+        focus: 'edit'
+      }
+    }
+  },
+  
+  edit: function() {
+    this.useEditor(function(editor) {
+      console.log('editor ready', editor)
+    })
+  },
+  
+  finish: function() {
+    
+  },
+  
+  getValue: function() {
+    if (this.editor) this.editor.updateElement();
+    return this.element.get('value')
+  }
   
 })
